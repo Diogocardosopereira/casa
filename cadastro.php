d1d1104b2887e0e1212fb9fe84074d19eb239c0f
diogo cardoso pereira <diogo.pereira9@aluno.ce.gov.br>
2022-05-29 14:29:53 -0300

    salva

---
+++ cadastro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+
+
+    <title>Cadastro</title>
+
+
+</head>
+  <body>
+  <center>
+  <div>
+  <h1>Cadastro</h1>
+  <form>
+         <div class="form-group">
+      <label for="name">Nome</label>
+      <input type="text" class="form-control" name="nome">
+     </div>
+     <br></br>
+     <div class="form-group">
+      <label for="name">Email</label>
+      <input type="Email" class="form-control" name="Email">
+     </div>
+     <br></br>
+     <div class="form-group">
+      <label for="login">Senha</label>
+      <input type="text" class="form-control" name="Senha">
+     </div>
+     <br></br>
+     <input type="submit">
+
+
+   </form>
+  </div>
+</center>
+</body>
+</html>
